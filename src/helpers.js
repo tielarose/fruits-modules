94b8e962a7958a629592d643685bd29ecc4cf645
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-12 22:02:30 -0700

    Create choice and remove functions

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+// returns a randomly selected item from array of items
+function choice(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+// removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
+function remove(items, item) {
+  const item_index = items.indexOf(item);
+  if (item_index >= 0) {
+    items.splice(item_index, 1);
+  }
+}
